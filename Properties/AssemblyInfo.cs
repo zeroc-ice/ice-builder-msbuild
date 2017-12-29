c9dfdcf96f80f8de760a88013ca7f812a0a2e982
Jose <jose@zeroc.com>
2017-12-29 20:06:22 +0100

    Initial commmit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+// **********************************************************************
+//
+// Copyright (c) 2009-2017 ZeroC, Inc. All rights reserved.
+//
+// **********************************************************************
+
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IceBuilder.MSBuild")]
+[assembly: AssemblyDescription("Ice Builder for MSBuild")]
+[assembly: AssemblyCompany("ZeroC, Inc.")]
+[assembly: AssemblyProduct("Ice Builder")]
+[assembly: AssemblyCopyright("Copyright (c) 2009-2017 ZeroC, Inc.")]
+[assembly: AssemblyVersion("5.0.0")]
+[assembly: ComVisibleAttribute(false)]
